64bbf505021880c7ca1a2e35e26e7c3935de90d2
zhenglinj <zhenglinj@hotmail.com>
2015-06-27 23:20:24 +0800

    Init

---
+++ bootstrap.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+platform=$(uname)
+echo "On $platform"
+
+echo ""
+echo "For common files ..."
+for fn in $(find . -maxdepth 1 -type f)
+do
+    if [[ ${fn} != *.cygwin && ${fn} != *.linux && ${fn} != bootstrap.sh ]]
+    then
+        fn=${fn##./}
+        echo "ln -fs `pwd`/${fn}  ~/.${fn}"
+        ln -fs `pwd`/${fn}  ~/.${fn}
+    fi
+done
+
+echo ""
+echo "For files in ~/.config ..."
+for fn in $(find ./config -type f)
+do
+    fn=${fn##./}
+    echo "ln -fs `pwd`/${fn}  ~/.${fn}"
+    ln -fs `pwd`/${fn}  ~/.${fn}
+done
+
+echo ""
+echo "For specific platform ($platform) ..."
+case $platform in
+    CYGWIN*)
+        for fn in $(find . -maxdepth 1 -type f -name "*.cygwin")
+        do
+            fn="${fn##./}"
+            fnn=".${fn%%.cygwin}"
+            echo "ln -fs `pwd`/${fn}  ~/${fnn}"
+            ln -fs `pwd`/${fn}  ~/${fnn}
+        done
+        ;;
+    Linux)
+        for fn in $(find . -maxdepth 1 -type f -name "*.linux")
+        do
+            fn="${fn##./}"
+            fnn=".${fn%%.linux}"
+            echo "ln -fs `pwd`/${fn}  ~/${fnn}"
+            ln -fs `pwd`/${fn}  ~/${fnn}
+        done
+        ;;
+    Darwin)
+        # TODO
+        ;;
+    *)
+        ;;
+esac
