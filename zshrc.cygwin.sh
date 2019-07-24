6a5943161ad0b677d6846271e918531cd369a9ed
Zheng Linjun <ljzheng64@best-inc.com>
2019-07-24 05:41:12 +0100

    Enable zsh docker plugins

--- zshrc.cygwin.sh
+++ zshrc.cygwin.sh
@@ -45,7 +45,11 @@ ZSH_THEME="robbyrussell"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git)
+plugins=(
+    docker
+    docker-machine
+    git
+)
 
 source $ZSH/oh-my-zsh.sh
 
